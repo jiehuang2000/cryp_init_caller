1d56b8ec17287ea185a15a4af69e7cb4093195f4
jiehuang2000 <jiehuang.sdu@gmail.com>
2016-10-26 13:19:25 -0400

    Update determine_cryptic_jumps_and_rates.R

--- determine_cryptic_jumps_and_rates.R
+++ determine_cryptic_jumps_and_rates.R
@@ -1,4 +1,4 @@
-#bsub Rscript ~/determine_cryptic_jumps_and_rates_faster.R ~/single_gene_seq.txt ~/output.txt ~/loss.txt 0
+#bsub Rscript ~/determine_cryptic_jumps_and_rates.R ~/single_gene_seq.txt ~/output.txt ~/loss.txt 0
 
 # args[1] == inputfile, args[2] == outfile1:genename, gene.len, theta, y, z, Diff , arg[3] == outfile, loss value of all sites
 # args[4] == 0/1, if we want stepsize=1, args[4] = 0, if want stepsize=100, args[4] == 1
