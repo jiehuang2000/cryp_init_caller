f66847dfd25040321ae1015e48adb995a066cbe1
jiehuang2000 <jiehuang.sdu@gmail.com>
2017-02-01 22:44:55 -0500

    Heatmap

---
+++ antisense_center_by_theta_heatmap_FixTh_FixGroup.R
@@ -0,0 +1,243 @@
+
+
+#submit code: 
+#0min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_0min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_WT_0min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt   
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_WT_0min_Rep4_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_WT_0min_Rep5_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_WT_0min_Rep6_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_SET2del_0min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_SET2del_0min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+#30min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_30min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_30min_Rep1_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_30min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_30min_Rep1_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_30min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_30min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+#60min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_60min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_60min_Rep1_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_60min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_60min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_60min_Rep1_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_60min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_60min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+#120min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_120min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_120min_Rep1_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_120min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_120min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_120min_Rep1_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_120min_Rep2_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_120min_Rep3_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+
+
+
+
+#0min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_0min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_WT_0min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt   
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R  yeast_SET2del_0min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+#30min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_30min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_30min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_30min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+#60min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_60min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_60min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_60min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+#120min
+#cd /proj/strahllb/users/Jie/Stephen/yeast_120min_antisenseStrand_full_genes_filledNAs_named.coord
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_WT_120min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt  
+#bsub -M 8 Rscript /proj/strahllb/users/Jie/Stephen/antisense_center_by_theta_heatmap_FixTh_FixGroup.R   yeast_SET2del_120min_Avg_antisenseStrand_full_genes_filledNAs_named.coord /proj/strahllb/users/Jie/Stephen/best_th_Group.txt 
+
+
+
+
+
+
+# for antisense file
+# args[1] == antisense file
+# args[2] == best th file with group info
+
+
+args = commandArgs(trailingOnly = TRUE)
+
+if (!require("gplots")) {
+   install.packages("gplots", dependencies = TRUE)
+   library(gplots)
+   }
+
+library(gplots)
+
+m <- matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE)
+png('test.png')
+heatmap.2(data.matrix(m))
+dev.off()
+
+
+data0=read.table(args[1],header=T,sep='\t')
+print("dimension of antisense read file:")
+
+if (grepl('Avg', args[1])) {
+   data0 = cbind(rownames(data0), data0)
+   colnames(data0)[1] = 'COORDID'
+} else {
+   rownames(data0) = data0$COORDID
+   }
+   
+dim(data0)
+print(colnames(data0)[1:10])
+
+
+
+
+
+
+
+#load in best theta result file
+theta=read.table(args[2],header=T,sep='\t')
+print("dimension of best_th_Group.txt  file:")
+dim(theta)
+head(theta)
+
+
+data=merge(theta, data0, by.x="Gene_Name", by.y="COORDID")
+print("dimension of the after merge with best_th_Group.txt file:")
+dim(data)
+head(data[,1:20])
+
+
+
+
+#at which column in the file starts to store reads info
+gene_start=which(colnames(data)=='Position1')
+
+
+n=nrow(data)
+
+antisense_center_theta = data[1:n ,1:(gene_start-1)]
+
+J=15000
+
+antisense_center_theta = cbind(antisense_center_theta, matrix(NA, n, 2*J))
+
+
+name=c(); 
+for (j in J:1) {name = c(name, paste0('upstream',j))}
+for (j in 0:(J-1)) {name = c(name, paste0('downstream',j))}
+
+
+colnames(antisense_center_theta)=c(colnames(antisense_center_theta)[1:(gene_start-1)], name)
+th_poi = which(colnames(antisense_center_theta) == 'downstream0')
+
+
+print("dimension of center_theta matrix :")
+dim(antisense_center_theta)
+head(antisense_center_theta[,1:20])
+
+
+for (i in 1:nrow(antisense_center_theta)) {
+  best_th = antisense_center_theta$Best_Theta[i]
+  if (is.na(best_th)) {
+    next
+  } else {
+    gene_len = antisense_center_theta$STOP[i]-antisense_center_theta$START[i]+1
+    antisense_center_theta[ i, (th_poi- best_th + 1) : (gene_len - best_th + th_poi )] = data[i, gene_start: (gene_start+gene_len-1)]
+  }
+}
+
+
+antisense_center_theta_ordered = antisense_center_theta[order(antisense_center_theta$Best_Theta),]
+
+
+
+antisense_center_theta_ordered_ge4 = antisense_center_theta_ordered[antisense_center_theta_ordered$Group == 'High' ,  ]
+antisense_center_theta_ordered_2to4 = antisense_center_theta_ordered[antisense_center_theta_ordered$Group == 'Medium' ,  ]
+antisense_center_theta_ordered_0to2 = antisense_center_theta_ordered[antisense_center_theta_ordered$Group == 'Low' ,  ]
+
+rownames(antisense_center_theta_ordered_ge4)=antisense_center_theta_ordered_ge4$Gene_Name
+rownames(antisense_center_theta_ordered_2to4)=antisense_center_theta_ordered_2to4$Gene_Name
+rownames(antisense_center_theta_ordered_0to2)=antisense_center_theta_ordered_0to2$Gene_Name
+
+all = rbind(antisense_center_theta_ordered_ge4, antisense_center_theta_ordered_2to4, antisense_center_theta_ordered_0to2)
+
+print(" antisense_center_theta_ordered_ge4 dimension:")
+dim(antisense_center_theta_ordered_ge4)
+
+print(" antisense_center_theta_ordered_2to4 dimension:")
+dim(antisense_center_theta_ordered_2to4)
+
+print(" antisense_center_theta_ordered_0to2 dimension:")
+dim(antisense_center_theta_ordered_0to2)
+
+
+# making .bed files for deeptools 
+# bed_ge4 = cbind(as.character(antisense_center_theta_ordered_ge4$CHROM), antisense_center_theta_ordered_ge4$START, antisense_center_theta_ordered_ge4$START+antisense_center_theta_ordered_ge4$Best_Theta - 1)
+# bed_2to4 = cbind(as.character(antisense_center_theta_ordered_2to4$CHROM), antisense_center_theta_ordered_2to4$START, antisense_center_theta_ordered_2to4$START+antisense_center_theta_ordered_2to4$Best_Theta - 1)
+# bed_0to2 = cbind(as.character(antisense_center_theta_ordered_0to2$CHROM), antisense_center_theta_ordered_0to2$START, antisense_center_theta_ordered_0to2$START+antisense_center_theta_ordered_0to2$Best_Theta - 1)
+
+# write.table(bed_ge4, file=paste0(strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])],'_ge4.bed'),sep='\t', row.names=F, col.names=F, quote=F)
+# write.table(bed_2to4, file=paste0(strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])],'_2to4.bed'),sep='\t', row.names=F, col.names=F, quote=F)
+# write.table(bed_0to2, file=paste0(strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])],'_0to2.bed'),sep='\t', row.names=F, col.names=F,quote=F)
+
+
+J=1000
+K=1000
+avg4_inf = colMeans (antisense_center_theta_ordered_ge4[,gene_start:ncol(antisense_center_theta_ordered_ge4)], na.rm = T, dims = 1)
+avg2_4 = colMeans (antisense_center_theta_ordered_2to4[,gene_start:ncol(antisense_center_theta_ordered_ge4)], na.rm = T, dims = 1)
+avg0_2 = colMeans(antisense_center_theta_ordered_0to2[,gene_start:ncol(antisense_center_theta_ordered_ge4)], na.rm= T, dims = 1)
+
+avg = rbind(avg4_inf,avg2_4,avg0_2)
+rownames(avg) = c('average4_inf', 'average2_4', 'average0_2')
+
+png(paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_upstream',J,'_downstream',K,'.png'), width = 5*600,  height = 5*600,  res = 600, pointsize = 8)  
+plot(-J:K, avg0_2[(15001-J):(15001+K)], type='l',col='red',ylim=c(0,300),ylab='average reads of antisense', xlab='position to best theta')
+points(-J:K,avg2_4[(15001-J):(15001+K)],type='l')
+points(-J:K,avg4_inf[(15001-J):(15001+K)],type='l',col='blue')
+legend('topright', c('4_inf','2-4','0-2'), col = c('blue','black','red'),pch = c(1, 1, 1), title='max loss diff')
+dev.off()
+
+write.table(antisense_center_theta_ordered_ge4, file=paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])],'_ge4.txt'),sep='\t', row.names=T, col.names=T, quote=F)
+write.table(antisense_center_theta_ordered_2to4, file=paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_2to4.txt'),sep='\t', row.names=T, col.names=T, quote=F)
+write.table(antisense_center_theta_ordered_0to2, file=paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_0to2.txt'),sep='\t', row.names=T, col.names=T, quote=F)
+
+write.table(avg[,(15001-J):(15001+K)], file=paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_upstream',J,'_downstream',K, '_average.txt'),sep='\t', quote=F, row.names=T, col.names=T)
+
+
+
+
+options(expressions=500000)
+
+L=3000
+
+my_palette <- colorRampPalette(c("white", "black"))(n = 100)
+
+png(paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_heatmap_ge4_upstream_downstream_',L,'.png'),width = 5*600,  height = 8*600,  res = 600, pointsize = 8)  
+heatmap.2(data.matrix(antisense_center_theta_ordered_ge4[,(th_poi-L):(th_poi+L)]),breaks=seq(0,100,length.out=101), dendrogram="none", trace="none",Rowv=FALSE, Colv=FALSE, col=my_palette, na.color="wheat")
+dev.off()
+
+
+png(paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_heatmap_2to4_upstream_downstream_',L,'.png'), width = 5*600,  height = 8*600,  res = 600, pointsize = 8)  
+heatmap.2(data.matrix(antisense_center_theta_ordered_2to4[,(th_poi-L):(th_poi+L)]),breaks=seq(0,100,length.out=101), dendrogram="none", trace="none",Rowv=FALSE, Colv=FALSE, col=my_palette, na.color="wheat")
+dev.off()
+
+png(paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_heatmap_0to2_upstream_downstream_',L,'.png'), width = 5*600,  height = 8*600,  res = 600, pointsize = 8)  
+heatmap.2(data.matrix(antisense_center_theta_ordered_0to2[,(th_poi-L):(th_poi+L)]),breaks=seq(0,100,length.out=101), dendrogram="none", trace="none",Rowv=FALSE, Colv=FALSE, col=my_palette, na.color="wheat")
+dev.off()
+
+
+png(paste0('fix_th_', strsplit(args[1], "/")[[1]][length(strsplit(args[1], "/")[[1]])], '_heatmap_all_upstream_downstream_',L,'.png'), width = 5*600,  height = 8*600,  res = 600, pointsize = 8)  
+heatmap.2(data.matrix(all[,(th_poi-L):(th_poi+L)]),breaks=seq(0,100,length.out=101), dendrogram="none", trace="none",Rowv=FALSE, Colv=FALSE, col=my_palette, na.color="wheat")
+dev.off()
+
+
